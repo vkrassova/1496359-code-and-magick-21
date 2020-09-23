c11577caa2b4ac3a57a96d2aeb6c802a301fa016
vkrassova <v.krassova@yandex.ru>
2020-09-23 22:41:13 +0300

    вносит правки по замечаниям

--- js/stat.js
+++ js/stat.js
@@ -1,47 +1,43 @@
 'use strict';
 
-const CLOUD_WIDTH = 420;
-const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
-const GAP = 50;
-const FONT_GAP = 15;
-const TEXT_WIDTH = 50;
-const BAR_HEIGHT = CLOUD_HEIGHT - GAP - TEXT_WIDTH - GAP + FONT_GAP;
+const CLOUD_HEIGHT = 270;
+const CLOUD_WIDTH = 420;
+const CLOUD_GAP = 20;
+const FONT_GAP = 10;
+const BAR_HEIGHT = 150;
 const BAR_WIDTH = 40;
-const MAX_HEIGHT = BAR_HEIGHT + GAP + GAP;
+const BAR_GAP = 50;
+const TITLE_X = CLOUD_X + CLOUD_GAP;
 
 const Font = {
-  SIZE: '16px',
+  SIZE: `16px`,
   FAMILY: `PT Mono`
 };
 
-const colorShodow = 'rgba(0, 0, 0, 0.5)';
+const colorShodow = `rgba(0, 0, 0, 0.5)`;
 const colorCloud = `#fff`;
 
-const getRandomNumber = function (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+
+const renderCloud = function (ctx) {
+  ctx.fillStyle = colorShodow;
+  ctx.fillRect(CLOUD_X + FONT_GAP, CLOUD_Y + FONT_GAP, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = colorCloud;
+  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-// const COLORS = [
-//   `hsl(0, 100%, 50%)`,
-//   `hsl(240, 100%, 40%)`,
-//   `hsl(238, 4%, 50%)`,
-//   `hsl(238, 20%, 50%)`
-// ];
-
-
-let renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+const textCloud = function (ctx, text, x, y) {
+  ctx.fillStyle = `#000`;
+  ctx.textBaseline = `hanging`;
+  ctx.font = `${Font.SIZE} ${Font.FAMILY}`;
+  ctx.fillText(text, x, y);
 };
 
-let getMaxElement = function (arr) {
-  let maxElement = arr[0];
+const getMaxElement = function (arr) {
+  const maxElement = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
+  for (let i = 1; i > arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -50,59 +46,60 @@ let getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx);
 
-  renderCloud(
+  textCloud(
       ctx,
-      CLOUD_X + 10,
-      CLOUD_Y + 10,
-      colorShodow
+      `Ура вы победили!`,
+      TITLE_X,
+      CLOUD_Y + CLOUD_GAP
   );
 
-  renderCloud(
+  textCloud(
       ctx,
-      CLOUD_X,
-      CLOUD_Y,
-      colorCloud
+      `Список результатов:`,
+      TITLE_X,
+      CLOUD_Y + BAR_WIDTH
   );
 
-  ctx.fillStyle = `#000`;
-  ctx.font = `${Font.SIZE} ${Font.FAMILY}`;
-  ctx.fillText(`Ура вы победили!`, CLOUD_X + FONT_GAP, CLOUD_Y + GAP - FONT_GAP);
-  ctx.fillText(`Список результатов:`, CLOUD_X + FONT_GAP, CLOUD_Y + GAP + FONT_GAP - 10);
-  
-
-  let maxTime = getMaxElement(times);
+  const maxTime = getMaxElement(times);
 
   for (let i = 0; i < names.length; i++) {
-    // let sumColor = 0;
-    // sumColor += i;
+    const BAR_X = CLOUD_X + BAR_GAP + (CLOUD_X - FONT_GAP) * i;
+    const BAR_SIZE = (-CLOUD_HEIGHT + BAR_HEIGHT + BAR_GAP + FONT_GAP) * times[i] / maxTime;
 
-    ctx.fillText(
-        names[i],
-        CLOUD_X + GAP - FONT_GAP + (GAP + BAR_WIDTH) * i,
-        CLOUD_Y + GAP + 200
-    );
+    // столбцы
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (names[i] === `Вы`) {
+      ctx.fillStyle = `rgba(255, 0, 0, 1)`;
     } else {
-      let random = getRandomNumber(1, 100);
-      ctx.fillStyle = 'hsl(240, ' + random + '%, 50%)';
+      ctx.fillStyle = `hsl(240, ` + Math.floor(Math.random() * 100) + `%, 50%)`;
     }
 
-    ctx.fillStyle = `#000`;
-    ctx.fillText(
-        Math.round(times[i]),
-        CLOUD_X + GAP - FONT_GAP + (GAP + BAR_WIDTH) * i,
-        MAX_HEIGHT - ((BAR_HEIGHT * times[i]) / maxTime)
+    ctx.fillRect(
+        BAR_X,
+        CLOUD_HEIGHT - CLOUD_GAP - FONT_GAP,
+        BAR_WIDTH,
+        BAR_SIZE
     );
 
-    // ctx.fillStyle = COLORS[sumColor];
-    ctx.fillRect(
-        CLOUD_X + GAP - FONT_GAP + (GAP + BAR_WIDTH) * i,
-        CLOUD_Y + GAP + 180,
-        BAR_WIDTH,
-        -((BAR_HEIGHT * times[i]) / maxTime)
+
+    // Время игроков
+
+    textCloud(
+        ctx,
+        Math.round(times[i]),
+        BAR_X,
+        BAR_SIZE + BAR_HEIGHT + BAR_GAP + CLOUD_GAP
+    );
+
+    // Имена игроков
+
+    textCloud(
+        ctx,
+        names[i],
+        BAR_X,
+        CLOUD_HEIGHT - CLOUD_GAP
     );
   }
 };
