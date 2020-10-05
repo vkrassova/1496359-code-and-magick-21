a2394bd90399c019153898c865d54807edd32db0
Victoria Krassova <v.krassova@yandex.ru>
2020-10-05 15:04:56 +0300

    начинает дз

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,7 @@
   const FIRST_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
   const SECOND_NAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
   const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-  const FIREBALL_WRAP = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  const FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 
   const userDialog = document.querySelector(`.setup`);
