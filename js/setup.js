639862e9c5eed4560e3ee6510974c34e4bcd859b
Victoria Krassova <v.krassova@yandex.ru>
2020-10-06 18:04:00 +0300

    добавляет переменные

--- js/setup.js
+++ js/setup.js
@@ -4,17 +4,26 @@
   const FIRST_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
   const SECOND_NAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
   const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
-  const FIREBALL_COLOR = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+  const FIREBALL_COLOR = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
   const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
 
   const userDialog = document.querySelector(`.setup`);
   userDialog.classList.remove(`hidden`);
 
   const similarListElement = userDialog.querySelector(`.setup-similar-list`);
-
   const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
     .content
     .querySelector(`.setup-similar-item`);
+
+  const setupWindow = document.querySelector(`.setup`);
+  const setupOpenIcon = document.querySelector(`.setup-open`);
+  const setupCloseButton = setupWindow.querySelector(`.setup-close`);
+  const setupWizard = setupWindow.querySelector(`.setup-wizard`);
+  const nameInputField = setupWindow.querySelector(`.setup-user-name`);
+  const wizardCoat = setupWizard.querySelector(`.setup-wizard .wizard-coat`);
+  const wizardEyes = setupWizard.querySelector(`.setup-wizard .wizard-eyes`);
+  const fireballWrapper = setupWizard.querySelector(`.setup-fireball-wrap`);
+
 
   const getRandomProperty = (array) => {
     return array[Math.floor(array.length * Math.random())];
